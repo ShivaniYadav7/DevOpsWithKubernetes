eb6c09bfc85d50b86f34a78daa821bfa35349be0
ShivaniYadav7 <shivaniyadav84237@gmail.com>
2026-08-28 23:01:04 +0530

    Complete project step3: Add React SPA

---
+++ the_project/my-spa/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App: React.FC = () => {
+  return (
+    <div style={{ fontFamily: 'Arial, sans-serif', textAlign: 'center', marginTop: '50px' }}>
+      <h1>Todo App</h1>
+      <p>Welcome to step 3 of the project! This SPA responds to GET requests at the root URL.</p>
+    </div>
+  )
+}
+
+export default App
